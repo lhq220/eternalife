7bcdcb20e146ea71ea4d29a1c8ee2ec0d73aab94
Lê Quân <hg.quan23@gmail.com>
2016-03-29 01:25:52 +0700

    Added files via upload

---
+++ js/app.js
@@ -0,0 +1,49 @@
+var myApp = angular.module('myApp', ['ngRoute']);
+
+myApp.config(function ($routeProvider) {
+    $routeProvider.when ('/', {
+        templateUrl : '/page/index.html',
+        controller: 'IndexController'
+    }).when ('/trang2', {
+        templateUrl : '/page/trang2.html',
+        controller : 'Trang2Controller'
+    }).otherwise({
+        redirectTo: '/'
+    });
+});
+myApp.factory('GetPost', function ($http) {
+    var factory = [];
+    factory.getTodos =  function () {
+        return $http.get('http://gellife.blogspot.com/feeds/posts/default');  
+    }
+    return factory;
+})
+myApp.controller('MasterController', ['$scope', function ($scope) {
+    'use strict';
+    $scope.name = 'Diễn đàn xe';
+    $scope.logo = '/images/sys/logo-ddx1.png';
+}]);
+myApp.controller('IndexController', ['$scope', '$http', 'GetPost', function ($scope, $http, GetPost) {
+    'use strict';
+    //data
+    $scope.baivietmoi = [];
+    loadbaiviet();
+    function loadbaiviet () {
+        var x2js = new X2JS();
+        GetPost.getTodos().success( function(data) {
+            courses =x2js.xml_str2json(data);
+            console.log(courses);
+        })
+    }
+    //$http.get('http://gellife.blogspot.com/feeds/posts/default')
+    //    .success( function (result) {
+    //        $scope.danhmucxe = result;
+    //    })
+    //    .error( function () {
+    //        $scope.danhmucxe  = '{ "ID" : "Lỗi" }';
+    //    });
+}]);
+myApp.controller('Trang2Controller', ['$scope', function ($scope) {
+    'use strict';
+
+}]);
