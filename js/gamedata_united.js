fa4bde0da998f595efdbc6e6704cb02615c417ae
Lê Quân <hg.quan23@gmail.com>
2016-03-29 10:36:49 +0700

    Added files via upload

---
+++ js/gamedata_united.js
@@ -0,0 +1,102 @@
+var data_game_united={
+	"lists":[
+		{
+            "cl":"taibai",
+            "united_name":"THÁI BẠCH",
+            "tw":"lưu tinh bạch vũ quang xuất hạp , nhất kiếm vô ngân tuyết mạn sơn",
+            "yw":"",
+            "level":4,
+            "fs":"",
+            "wp":[
+                ["kiếm"],
+                ["bao kiếm"]
+            ], 
+            "lk":"#",
+            "gl":"#"
+        },
+		{
+            "cl":"shenwei",
+            "united_name":"THẦN UY",
+            "tw":"thần uy đảo quyển phiên không lãng , nhất cử trùng tiêu khí thế hùng",
+            "yw":"",
+            "level":3,
+            "fs":"",
+            "wp":[
+                ["thương"],
+                ["trường cung"] 
+            ],
+            "lk":"#",
+            "gl":"#"
+        },
+		{
+            "cl":"tangmen",
+            "united_name":"ĐƯỜNG MÔN",
+            "tw":"vụ ải vân tòng phi tinh lạc , bán ảnh tương y phiến trung tình",
+            "yw":"",
+            "level":5,
+            "fs":"̀",
+            "wp":[
+                ["khôi lỗi"],
+                ["phiến tử"]
+            ],            
+            "lk":"#",
+            "gl":"#"
+        },
+		{
+            "cl":"gaibang",
+            "united_name":"CÁI BANG",
+            "tw":"túy lai hào khí bất khả thu , hư tác trường hồng quán ngưu đấu",
+            "yw":"",
+            "level":4,
+            "fs":"",
+            "wp":[
+                ["triền thủ"],
+                ["tửu hồ lô"]
+            ], 
+            "lk":"#",
+            "gl":"#"
+            },
+		{
+            "cl":"tianxiang",
+            "united_name":"THIÊN HƯƠNG",
+            "tw":"cạnh khoa thiên hạ song vô tuyệt , độc lập nhân gian đệ nhất hương",
+            "yw":"",
+            "level":3,
+            "fs":"",
+            "wp":[
+                ["tán"],
+                ["tán trung kiếm"]
+            ],
+            "lk":"#",
+            "gl":"#"
+        },
+		{
+            "cl":"zhenwu",
+            "united_name":"CHÂN VÕ",
+            "tw":"linh diệu tiềm thông thừa phong khởi , thái cực huyền hư nhược kính thanh",
+            "yw":"",
+            "level":4,
+            "fs":"",
+            "wp":[
+                ["song kiếm"],
+                ["kiếm sao"]
+            ],
+            "lk":"#",
+            "gl":"#"
+        },
+		{
+            "cl":"wudu",
+            "united_name":"NGŨ ĐỘC",
+            "tw":"động du bích lạc tâm vô ngại , tĩnh tàng thâm uyên tích tuyệt tung",
+            "yw":"",
+            "level":5,
+            "fs":"",
+            "wp":[
+                ["song nhận"],
+                ["song nhận"]
+            ],
+            "lk":"#",
+            "gl":"#"
+        }
+	]	
+};
